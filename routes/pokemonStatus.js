637fff140abce0d1d96c66c4292b3a10e25832dc
fcortesbio <fcortesbio@gmail.com>
2025-01-10 08:41:29 -0500

    Define test route in pokemonStatus

---
+++ routes/pokemonStatus.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/test', (req, res)=>{
+    res.send('Hello, Router!').status(200)
+    console.log('Succesfully reached pokemonStatus!')
+})
+
+module.exports = router
+
