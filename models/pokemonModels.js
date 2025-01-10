4dbde672fd122d485923217b8b0a224dcb26456d
fcortesbio <fcortesbio@gmail.com>
2025-01-10 10:04:27 -0500

    created mongoose Schema for pokemonStatus

--- models/pokemonModels.js
+++ models/pokemonModels.js
@@ -1,1 +1,27 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema();
+
+const PokemonSchema = new Schema({
+  pokemon_id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+
+  encounter: {
+    type: Boolean,
+    default: false,
+  },
+
+  catch: {
+    type: Boolean,
+    default: false,
+  },
+
+  team_member: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("pokemonStatus", PokemonSchema);
