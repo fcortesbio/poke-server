fa66c89db370028a3411e8aafbef0285094419d5
fcortesbio <fcortesbio@gmail.com>
2025-01-10 09:17:08 -0500

    updated string formatting

--- routes/pokemonStatus.js
+++ routes/pokemonStatus.js
@@ -1,10 +1,9 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
-router.get('/test', (req, res)=>{
-    res.send('Hello, Router!').status(200)
-    console.log('Succesfully reached pokemonStatus!')
-})
+router.get("/test", (req, res) => {
+  res.send("Hello, Router!").status(200);
+  console.log("Succesfully reached pokemonStatus!");
+});
 
-module.exports = router
-
+module.exports = router;
