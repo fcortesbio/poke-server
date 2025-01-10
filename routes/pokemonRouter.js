d3a558d2365267b1bb3067426242d5e383cac3a0
fcortesbio <fcortesbio@gmail.com>
2025-01-10 12:23:32 -0500

    Add path for createPokemonStatus in routes

--- routes/pokemonRouter.js
+++ routes/pokemonRouter.js
@@ -2,17 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 // Import the 'test' controller function from the pokemonController
-const { test } = require("../controllers/pokemonController"); 
+const { test, createPokemonStatus } = require("../controllers/pokemonController"); 
 
 // Define the main API route (GET request to '/')
 router.get("/", (req, res) => {
-  // Send a "Hello, Router!" response with a 200 (OK) status code
   res.status(200).send("Hello, Router!"); 
   console.log("Successfully reached Routes"); 
 });
 
-// Define a route to the 'test' controller function (GET request to '/test')
+// Define routes to controller functions
 router.get("/test", test); 
+router.post("/create", createPokemonStatus)
+
 
 // Export router to be reached by index.js
 module.exports = router;
