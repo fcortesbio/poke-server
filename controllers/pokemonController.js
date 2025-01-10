7db7aaee8804aff706df1ff832e56729223aad28
fcortesbio <fcortesbio@gmail.com>
2025-01-10 10:32:40 -0500

    Add controller for createPokemonStatus; to-do: create a route for handling post requests using this controller.

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -2,3 +2,21 @@ exports.test = (req, res) => {
   res.status(200).send("Hello, Controller!");
   console.log("Successfully reached Controllers");
 };
+
+const pokemonStatus = require("../models/pokemonModels");
+
+exports.createPokemonStatus = async (req, res) => {
+  try {
+    const status = await pokemonStatus.create({
+      pokemon_id: req.body.pokemon_id,
+      encounter: req.body.encounter,
+      catch: req.body.catch,
+      team_member: req.body.team_member,
+    });
+    res.status(201).json(status);
+    console.log(`Created pokemonStatus for: Pokemon ID: ${req.body.pokemon_id}`);
+  } catch (err) {
+    console.error(`Unable to create pokemon Status: ${err}`);
+    res.status(500).json({ err });
+  }
+};
