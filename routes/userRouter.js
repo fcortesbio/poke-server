60e88208a20a01d008e3c6f3ea5d4676e8516524
fcortesbio <fcortesbio@gmail.com>
2025-01-12 04:06:09 -0500

    feat(auth): Add user authentication routes
    This commit adds routes for user authentication and a protected
    route that requires authentication.
    
    - Defines routes for user signup, username check, and password
      check, mapping them to the corresponding controller functions.
    - Adds a protected  as an example route that uses the
       middleware to ensure only authenticated
      users can access it.
    
    This sets up the necessary routes for user authentication and
    demonstrates how to protect routes using JWT-based authentication.

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, checkUsername, checkPassword } = require("../controllers/userController");
+const {
+  registerUser,
+  checkUsername,
+  checkPassword,
+} = require("../controllers/userController");
+const { authenticateToken } = require("../middleware/cookieJwtAuth");
 
-// Route for registration
-router.post("/signup", registerUser); 
+// -- User Authentication Handling routes: --
+router.post("/signup", registerUser); // sign up a new user (CREATE)
+router.post("/login/username", checkUsername); // check username
+router.post("/login/password", checkPassword); // check username+password
 
-// Route for login username check
-router.post("/login/username", checkUsername); 
-
-// Route for password check (username validated)
-router.post("/login/password", checkPassword); 
+// --- JWT protected routes:
+router.get("profile/", authenticateToken, (req, res) => {
+  res.json({ user: req.user });
+  console.log("route profile reached!");
+});
 
 module.exports = router;
