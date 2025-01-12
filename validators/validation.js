946973af8aaaabc5543851c8afaca01a161e8790
fcortesbio <fcortesbio@gmail.com>
2025-01-11 22:08:25 -0500

    feat(auth): Add Joi validation for user registration and login Introduce input validation for use registration and login requests using the Joi library. - Define schemas for registration and login data - Export validation functions (, ) to be used in route handlers This improves data integrity and security by ensuring that incoming data meets the expected format and constraints.

---
+++ validators/validation.js
@@ -0,0 +1,31 @@
+const joi = require("joi");
+
+// validation schema for user registration
+
+const registerSchema = joi.object({
+    username: joi.string().min(6).max(25).required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(8).required(),
+    roles: joi.array().items(joi.string()).default(["trainer"]),
+});
+
+// validation schema for user login 
+
+const loginSchema = joi.object({
+    email: joi.string().email().required();
+    password: joi.string().min(8).required(),
+});
+
+const validateRegistration = (data) => {
+    return registerSchema.validate(data)
+};
+
+const validateLogin = (data) => {
+    return loginSchema.validate(data)
+};
+
+module.exports = {
+    validateRegistration,
+    validateLogin,
+};
+
