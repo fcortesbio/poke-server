4572385a58a1dddc784ba63727e0a0d59b1ff235
fcortesbio <fcortesbio@gmail.com>
2025-01-12 06:59:22 -0500

    fix: corrected spelling for route name

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,6 +4,7 @@ const {
   registerUser,
   checkUsername,
   checkPassword,
+  logoutUser,
 } = require("../controllers/userController");
 const { authenticateToken } = require("../middleware/cookieJwtAuth");
 
@@ -15,9 +16,10 @@ router.get("/", (req, res) => res.status(200).send("Hello, User!"));
 router.post("/signup", registerUser); // sign up a new user (CREATE)
 router.post("/login/", checkUsername); // check username
 router.post("/login/pass", checkPassword); // check username+password
+router.post("/logout", logoutUser); // Logout route
 
 // --- JWT protected routes:
-router.get("profile/", authenticateToken, (req, res) => {
+router.get("/profile", authenticateToken, (req, res) => {
   res.json({ user: req.user });
   console.log("route profile reached!");
 });
