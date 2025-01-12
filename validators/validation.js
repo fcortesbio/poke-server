1e49c303aecf93b5d4e7bf465f79fdcd07ea0de2
fcortesbio <fcortesbio@gmail.com>
2025-01-12 00:22:38 -0500

    refactor(auth): Update validation schemas for new login flow
    This commit updates the validation schemas to support the new
    two-step login process.
    
    - Removes the  and  function, as they
      are no longer needed.
    - Updates the  and  to reflect
      the specific requirements of the new login flow.
    
    This change ensures that the validation logic aligns with the
    updated authentication process.

--- validators/validation.js
+++ validators/validation.js
@@ -3,7 +3,7 @@ const joi = require("joi");
 // validation schema for user registration
 
 const registerSchema = joi.object({
-  username: joi.string().min(3).max(30).alphanum().required(), // Alphanumeric usernames
+  username: joi.string().min(6).max(25).alphanum().required(), 
   email: joi.string().email().required(),
   password: joi
     .string()
@@ -17,24 +17,45 @@ const registerSchema = joi.object({
     .default(["trainer"]),
 });
 
-// validation schema for user login
-
-const loginSchema = joi.object({
-  email: joi.string().email().required(),
-  password: joi.string().min(8).required(),
+// validation schema for username
+const usernameSchema = joi.object({
+  username: joi.string(6).min(25).alphanum().required,
 });
+
+// validation schema for password (asked once username is found)
+const passwordSchema = joi.object({
+  password: joi.string(8).required(),
+});
+
+// validation schema for user login
+// const loginSchema = joi.object({
+//   email: joi.string().email().required(),
+//   password: joi.string().min(8).required(),
+// });
+
+// const validateLogin = (data) => {
+//   const { error, value } = loginSchema.validate(data);
+//   return { error, value };
+// };
 
 const validateRegistration = (data) => {
   const { error, value } = registerSchema.validate(data);
   return { error, value };
 };
-const validateLogin = (data) => {
-  const { error, value } = loginSchema.validate(data);
-  return { error, value };
-};
+
+const validateUsername = (data) => {
+  const {error, value} = usernameSchema.validate(data)
+  return {error, value}
+}
+
+const validatePassword = (data) => {
+  const {error, value} = passwordSchema.validate(data);
+  return {error, value}
+}
 
 // Export the validation functions to be used by routers
 module.exports = {
   validateRegistration,
-  validateLogin,
+  validateUsername,
+  validatePassword
 };
