8c6bbbf936e3a3812089fab3d13100e350726213
fcortesbio <fcortesbio@gmail.com>
2025-01-11 21:42:14 -0500

    feat(auth): Define User model with security features
    Creates the User model with the following features:
    
    - Username and email fields for login.
    - Password hashing using bcrypt.
    - Roles for authorization.
    - Fields for email verification and password resets.

---
+++ models/users.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  roles: {
+    type: [String],
+    default: ["trainer"],
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  updatedAt: {
+    type: Date.now,
+    default: null,
+  },
+
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+
+  verificationToken: {
+    type: String,
+    default: null,
+  },
+
+  passwordResetToken: {
+    type: String,
+    default: null,
+  },
+
+  passwordResetExpires: {
+    type: Date,
+    default: null,
+  },
+});
+
+// Hash the password before saving
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next(); 
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    try {
+        return await bcrypt.compare(candidatePassword, this.password);
+    } catch (err) {
+        throw err;
+    }
+};
+
+module.exports = mongoose.model("userAuth", userSchema);
