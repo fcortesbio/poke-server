a26a4a827e801e08fb93a398c1c65126b2475846
fcortesbio <fcortesbio@gmail.com>
2025-01-12 04:04:47 -0500

    feat(auth): Implement user authentication logic
    This commit implements the core logic for user authentication,
    including username and password validation, JWT generation, and
    session management.
    
    - Adds  function to validate usernames and store
      them in the session.
    - Adds  function to validate passwords, compare
      them with stored hashes, and generate JWTs upon successful
      authentication.
    - Integrates JWT generation and cookie setting into the
      authentication flow.
    
    This completes the essential functionality for user login and
    session management.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,64 +1,67 @@
 const {
-    validateRegistration, 
-    validateUsername, 
-    validatePassword    
+  validateRegistration,
+  validateUsername,
+  validatePassword,
 } = require("../validators/validation");
+const { generateToken } = require("../middleware/cookieJwtAuth");
 
 const User = require("../models/users");
 
-
 const registerUser = async (req, res) => {
-    const {error, value} = validateRegistration(req.body);
-    if (error) return res.status(400).json({error: error.details[0].message});
+  const { error, value } = validateRegistration(req.body);
+  if (error) return res.status(400).json({ error: error.details[0].message });
 
-    try {
-        const user = new User(value);
-        await user.save();
-        res.status(201).json({message: "New user registered"});
-    } catch (err) {
-        res.status(500).json({error: err.message})
-    }
-}
+  try {
+    const user = new User(value);
+    await user.save();
+    res.status(201).json({ message: "New user registered" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
-const checkUsername = async(req, res) => {
-    const {error, value} = validateUsername(req.body);
-    if (error) return res.status(400).json({error: error.details[0].message});
+const checkUsername = async (req, res) => {
+  const { error, value } = validateUsername(req.body);
+  if (error) return res.status(400).json({ error: error.details[0].message });
 
-    try {
-        const user = await User.findOne({username: value.username});
-        if (!user) {return res.status(404).json({error: error.details[0].message})};
-        res.json({message: "Username found"})
-    } catch (err) {
-        res.status(500).json({error: err.message})
-    }
-}
+  try {
+    const user = await User.findOne({ username: value.username });
+    if (!user) return res.status(404).json({ error: error.details[0].message });
+    req.session.username = value.username;
+    res.json({ message: "Username found and stored" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
 const checkPassword = async (req, res) => {
-    const {error, value} = validatePassword(req.body);
-    if (error) return res.status(400).json({error: error.details[0].message});
+  const { error, value } = validatePassword(req.body);
+  if (error) return res.status(400).json({ error: error.details[0].message });
 
-    try {
-        const username = req.session.username;
-        const user = await User.findOne({username: username});
+  try {
+    const username = req.session.username;
+    const user = await User.findOne({ username: username });
 
-        if (!user){return res.status(404).json({error: error.details[0].message})};
-
-        const isMatch = await user.comparePassword(value.password);
-
-        if (!isMatch){
-            return res.status(401).json({error: "Incorrect access"});
-        }
-
-        /// ... generate and send JWT
-
-    } catch (error) {
-        res.status(500).json({error: err.message});
+    if (!user) {
+      return res.status(404).json({ error: error.details[0].message });
     }
-}
 
+    const isMatch = await user.comparePassword(value.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ error: "Incorrect access" });
+    }
+
+    const token = generateToken(user);
+    res.cookie("token", token);
+    res.json({ message: "User logged in" });
+  } catch (error) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
 module.exports = {
-    registerUser,
-    checkUsername,
-    checkPassword
-}
+  registerUser,
+  checkUsername,
+  checkPassword,
+};
